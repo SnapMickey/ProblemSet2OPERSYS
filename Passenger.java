517aeb0f9092155cfd39a7299d64086acf923adb
SnapMickey <jared_rivera@dlsu.edu.ph>
2017-07-16 16:15:37 +0800

    EXOSKELE TON OF DATA RIGHT THERE

---
+++ Passenger.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author Jared
+ * @version 0
+ * 
+ * Thread class for Robot Passengers.
+ */
+public class Passenger {
+    
+}
---
+++ Train.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author Jared
+ * @version 0
+ * 
+ * Thread class for Trains.
+ */
+public class Train {
+    
+}
